358f864c78317eaed86b1faed0d8c1a962e76fb4
Busolin <victorbusolin@alunos.utfpr.edu.br>
2022-05-27 09:42:35 -0300

    Exibição de usuários no localStorage feito

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,8 +1,29 @@
 import { Component, OnInit } from "@angular/core";
+import { User } from "../User";
+import { LoginComponent } from "../login/login.component";
 
 @Component({
   selector: "lista-users",
   templateUrl: "./lista.component.html",
   styleUrls: ["./lista.component.css"],
 })
-export class ListaUsers {}
+export class ListaUsers implements OnInit {
+  users: User[];
+
+  ngOnInit(): void {
+    this.ExibirProdutos();
+  }
+
+  ExibirProdutos(): void {
+    if (localStorage.getItem("BD")) {
+      this.users = JSON.parse(localStorage.getItem("BD"));
+    } else {
+      this.users = [];
+    }
+  }
+
+  deleteUser(user, i) {
+    const index = this.users.indexOf(user);
+    console.log(user, i);
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
   formulario: any;
 
   ngOnInit(): void {
+    this.ExibirProdutos();
     this.formulario = new FormGroup({
       userId: new FormControl(),
       nomeCompleto: new FormControl(),
